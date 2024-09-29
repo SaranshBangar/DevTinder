f3ad8c9087500ee0ee940eeda9188c487fc4a981
Saransh Bangar <saranshbangad@gmail.com>
2024-09-29 20:13:02 +0530

    Improved routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,20 +2,47 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send("DevTinder");
+// : is used to define a route parameter
+app.get("/user/:userId", (req, res) => {
+
+    // console.log(req.query); // It will print the query params
+
+    console.log(req.params)
+
+    res.send({firstName: "Dev", lastName: "Singh"});
 })
 
-app.use("/hello", (req, res) => {
-    res.send("Hello from the server!");
+// This will match /user and /uer
+// app.get("/us?er", (req, res) => {
+//     res.send({firstName: "Dev", lastName: "Singh"});
+// })
+
+// This will match /user, /usser, /ussser, /usssser, and so on. 
+// app.get("/us+er", (req, res) => {
+//     res.send({firstName: "Dev", lastName: "Singh"});
+// })
+
+// This will match /user, /usabcder, /usxyzer, /usrrr, and so on.
+// app.get("/us*er", (req, res) => {
+//     res.send({firstName: "Dev", lastName: "Singh"});
+// })
+
+app.post("/user", (req, res) => {
+    console.log("Save user to database!");
+    res.send("User is saved successfully!");
+})
+
+app.patch("/user", (req, res) => {
+    console.log("Update user to database!");
+    res.send("User is updated successfully!");
+})
+
+app.delete("/user", (req, res) => {
+    res.send("Deleted successfully!");
 })
 
 app.use("/test", (req, res) => {
     res.send("Server is testing!");
-})
-
-app.use("/sample", (req, res) => {
-    res.send("Sample response from the server!");
 })
 
 app.listen(3000, () => {
