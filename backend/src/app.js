67558689049eb31fd25719d3b5e942cac9e5d50a
Saransh Bangar <saranshbangad@gmail.com>
2024-09-30 19:00:54 +0530

    error handeling

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,22 +3,34 @@ const { adminAuth, userAuth } = require("./middlewares/auth");
 
 const app = express();
 
-app.use("/admin", adminAuth);
+// app.use("/admin", adminAuth);
 
-app.get("/admin/getData", (req, res) => {
-    res.send("Fetched all data!");
+// app.get("/admin/getData", (req, res) => {
+//     res.send("Fetched all data!");
+// });
+
+// app.get("/admin/deleteData", (req, res) => {
+//     res.send("Deleted data!");
+// });
+
+// app.use("/user/login", (req, res) => {
+//     res.send("User login successfull!");
+// });
+
+// app.use("/user", userAuth, (req, res) => {
+//     res.send("Accessed user data!");
+// });
+
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        res.status(500).send("Something went wrong!");
+    } else {
+        next();
+    }
 });
 
-app.get("/admin/deleteData", (req, res) => {
-    res.send("Deleted data!");
-});
-
-app.use("/user/login", (req, res) => {
-    res.send("User login successfull!");
-});
-
-app.use("/user", userAuth, (req, res) => {
-    res.send("Accessed user data!");
+app.get("/", (req, res) => {
+    res.send("User data fetched successfully!");
 });
 
 app.listen(3000, () => {
