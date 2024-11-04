b4a3849ac47eeb18f056e11754b5e58094a6deba
Saransh Bangar <saranshbangad@gmail.com>
2024-11-04 15:53:14 +0530

    vercel - 3

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,7 +1,7 @@
-export const BASE_AUTH_URL = 'http://localhost:3000/auth';
+export const BASE_AUTH_URL = process.env.REACT_APP_BACKEND_URL + 'auth';
 
-export const BASE_PROFILE_URL = 'http://localhost:3000/profile';
+export const BASE_PROFILE_URL = process.env.REACT_APP_BACKEND_URL + 'profile';
 
-export const BASE_USER_URL = 'http://localhost:3000/user';
+export const BASE_USER_URL = process.env.REACT_APP_BACKEND_URL + 'user';
 
-export const BASE_REQUEST_URL = 'http://localhost:3000/request';
+export const BASE_REQUEST_URL = process.env.REACT_APP_BACKEND_URL + 'request';
